3ab855d7bcab6497fd14b27f81e5108dc7d5573a
tygszfa <185154684+genizy@users.noreply.github.com>
2025-05-18 14:52:39 -0700

    add freds

---
+++ master-loader.js
@@ -0,0 +1,11 @@
+if (!window.config.unityWebglLoaderUrl) {
+    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/gn-math/assets@main/UnityLoader.2019.2.js";
+}
+let sdkScript = document.createElement("script");
+sdkScript.src = "https://cdn.jsdelivr.net/gh/gn-math/assets@main/poki-sdk.js";
+sdkScript.onload = function() {
+    let i = document.createElement("script");
+    i.src = "https://cdn.jsdelivr.net/gh/gn-math/assets@main/unity.js";
+    document.body.appendChild(i)
+};
+document.body.appendChild(sdkScript);
