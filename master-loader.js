583f35ff9d5e05952db84a0efc743761fcdd44f5
tygszfa <185154684+genizy@users.noreply.github.com>
2025-05-18 15:10:34 -0700

    update master loader

--- master-loader.js
+++ master-loader.js
@@ -1,5 +1,5 @@
 if (!window.config.unityWebglLoaderUrl) {
-    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/gn-math/assets@2bca57e32f8a6b67ea5d415fd7b6068ebf00d0be/UnityLoader.2019.2.js";
+    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/gn-math/assets@7b475280c96633a3dddcc7c138390b23825267b1/UnityLoader.2019.2.js";
 }
 let sdkScript = document.createElement("script");
 sdkScript.src = "https://cdn.jsdelivr.net/gh/gn-math/assets@main/poki-sdk.js";
