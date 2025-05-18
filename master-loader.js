3875f17541fa2cb803f5429e1eeb522c19f6c18d
tygszfa <185154684+genizy@users.noreply.github.com>
2025-05-18 15:06:36 -0700

    actually update hash this time

--- master-loader.js
+++ master-loader.js
@@ -1,5 +1,5 @@
 if (!window.config.unityWebglLoaderUrl) {
-    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/gn-math/assets@8676807bfa6275b6303d1a5e57df4550051d1f38/UnityLoader.2019.2.js";
+    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/gn-math/assets@2bca57e32f8a6b67ea5d415fd7b6068ebf00d0be/UnityLoader.2019.2.js";
 }
 let sdkScript = document.createElement("script");
 sdkScript.src = "https://cdn.jsdelivr.net/gh/gn-math/assets@main/poki-sdk.js";
